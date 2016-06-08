4f4ef2424e61f5a7dc87c072a5196df818b7a784
Luis Angel Perez Muñoz <ingluisperez.m@outlook.com>
2016-06-07 20:36:18 -0500

    Agregue el Boton Salir
    Agregue en la barra de titulo el boton Salir
    Se sale de la sesion actual y regresa a la pantalla de login

--- MyApplication/app/src/main/java/com/example/luis/solicitudveranos/home.java
+++ MyApplication/app/src/main/java/com/example/luis/solicitudveranos/home.java
@@ -159,6 +159,23 @@ public class home extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.action_salir){
+            sesion.unauth();
+            Intent intent = new Intent(home.this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
 
