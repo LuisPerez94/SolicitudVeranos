eb5d1cf281de34556a3d830e2f20f81add3fcb8e
sterben93 <rous.archer@gmail.com>
2016-06-07 20:15:44 -0500

    actualizacion

--- web/php/download-xls.php
+++ web/php/download-xls.php
@@ -8,14 +8,11 @@ date_default_timezone_set('Europe/London');
 if (PHP_SAPI == 'cli')
     die('This example should only be run from a Web Browser');
 
-/** Include PHPExcel */
 require_once 'PHPExcel.php';
 
 
-// Create new PHPExcel object
 $objPHPExcel = new PHPExcel();
 
-// Set document properties
 $objPHPExcel->getProperties()->setCreator("Coordinadores de Carreras")
         ->setLastModifiedBy("Coordinadores de Carreras")
         ->setTitle("Office 2007 XLSX Test Document")
@@ -24,32 +21,50 @@ $objPHPExcel->getProperties()->setCreator("Coordinadores de Carreras")
         ->setKeywords("office 2007 openxml php")
         ->setCategory("Test result file");
 
-
-// Add some data
 $objPHPExcel->setActiveSheetIndex(0)
         ->setCellValue('A1', "Tecnológico Nacional de México Instituto Tecnológico de Veracruz")
         ->setCellValue('A2', "Materias mas solicitadas en para verano");
+$styleArray = array(
+    'font' => array(
+        'bold' => true,
+        'size'=>12,
+    ),
+    'alignment' => array(
+        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+    ),
+    'borders' => array(
+        'bottom' => array('style' => PHPExcel_Style_Border::BORDER_THIN),
+        'top' => array('style' => PHPExcel_Style_Border::BORDER_THIN),
+        'lef' => array('style' => PHPExcel_Style_Border::BORDER_THIN),
+        'right' => array('style' => PHPExcel_Style_Border::BORDER_THIN)
+    ),
+);
+
+$objPHPExcel->getActiveSheet()->getStyle('A1:G1')->applyFromArray($styleArray);
 $objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
 $objPHPExcel->getActiveSheet()->getRowDimension('1')->setRowHeight(20);
 $objPHPExcel->getActiveSheet()->getStyle()->getFont()->setSize(20);
 $objPHPExcel->getActiveSheet()->mergeCells('A1:G1');
+$objPHPExcel->getActiveSheet()->getStyle()->getFont()->setSize(20);
+$objPHPExcel->getActiveSheet()->getRowDimension('4')->setRowHeight(20);
 $objPHPExcel->getActiveSheet()->getCell('A4')->setValue("Materias");
-$objPHPExcel->getActiveSheet()->getCell('C4')->setValue("Solicitante");
-
+$objPHPExcel->getActiveSheet()->getCell('B4')->setValue("Solicitante");
+$objPHPExcel->getActiveSheet()->getStyle('A1:G1')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+$objPHPExcel->getActiveSheet()->getStyle('A4:B4')->applyFromArray($styleArray);
 $arrayMaterias = $_POST["id"];
-$materias =  preg_split("/[,]+/", $arrayMaterias);
+$materias = preg_split("/[,]+/", $arrayMaterias);
 $arrayAlumnos = $_POST["alumnos"];
 $alumnos = preg_split("/[,]+/", $arrayAlumnos);
 //
-$index=5;
-$index2=0;
-foreach ($materias as $materia){
-    $objPHPExcel->getActiveSheet()->getCell('A'.$index)->setValue($materia);
-    $nombres=  preg_split('/<br[^>]*>/i', $alumnos[$index2++]);
-        foreach($nombres as $nombre){
-            $objPHPExcel->getActiveSheet()->getCell('C'.$index++)->setValue($nombre);
-        }
-
+$index = 5;
+$index2 = 0;
+foreach ($materias as $materia) {
+    $objPHPExcel->getActiveSheet()->getCell('A' . $index)->setValue($materia);
+    $nombres = preg_split('/<br[^>]*>/i', $alumnos[$index2++]);
+    foreach ($nombres as $nombre) {
+        $objPHPExcel->getActiveSheet()->getCell('B' . $index++)->setValue($nombre);
+    }
 }
 $objPHPExcel->getActiveSheet()->setTitle('Veranos');
 $objPHPExcel->setActiveSheetIndex(0);
